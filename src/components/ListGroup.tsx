47be47e08e7c8bfa2cd14233614dfc5e38a363e7
chioma <chiomabiodun@outlook.com>
2023-04-07 00:00:35 +0100

    event handler functions

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,20 +1,33 @@
-function ListGroup() {
-    const items = [
-        'Paris',
-        'Newyork',
-        'Tokyo',
-        'Nigeria'
-    ] ;
+import { MouseEvent } from "react";
 
-    
+function ListGroup() {
+  let items = ["Paris", "Newyork", "Tokyo", "Nigeria"];
+  //let selectedItem = 0;
+  // items = [];
+
+  /* const getMessage = () => {
+        return items.length === 0 ? <p> No item found</p> : null
+    } */
+
+    //Event handler
+    const handleClick = (event: MouseEvent) => console.log(event);
+
   return (
     <>
-    <h1>List of items</h1>
-    <ul className="list-group">
-    {items.map(item => 
-       <li key = {item}>{item}</li>
-    )}
-    </ul>
+      <h1>List of items</h1>
+      {/* {getMessage()} */}
+      {items.length === 0 && <p> No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={handleClick}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
